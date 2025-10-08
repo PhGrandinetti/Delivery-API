0e53dacbce3bb5a9548a3f4b6384f7ef7a3ddf9b
Paulo Grandinetti <linho.leite@gmail.com>
2025-10-08 10:09:15 -0300

    Adicionado middleware de error

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import userRouter from './routes/user.router.js'
+import { globalErrorHandler } from './middlewares/error.middleware.js'
 
 const porta = 3000
 const app = express()
@@ -8,6 +9,7 @@ app.use(express.json())
 
 app.use('/api/user', userRouter)
 
+app.use(globalErrorHandler)
 
 app.listen(porta, () => {
     console.log(`Servidor rodando na porta ${porta}`)
