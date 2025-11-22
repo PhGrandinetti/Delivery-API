8858ae11b8d5a0b7c53fa0dc4052841614c39421
Paulo Henrique <linho.leite@gmail.com>
2025-11-22 16:37:58 -0300

    Refatorado repositórios(user, menu)

--- src/repositories/menu.repository.js
+++ src/repositories/menu.repository.js
@@ -1,84 +1,28 @@
-import db from '../config/dataBase.js'
-import {v4 as uuidv4} from 'uuid'
+import Menu from "../models/menu.model.js"
 
 //Repositório dos itens do menu. Com as funções de criar, deletar, alterar, mostrar e achar por ID/Nome.
 class MenuRepository{
-    static async create(menuData){
-        await db.read()
-        
-        const newMenu = {
-            id: uuidv4(),
-            nome: menuData.nome,
-            ingredientes: menuData.ingredientes,
-            preco: menuData.preco,
-            observacoes: menuData.observacoes
-        }
 
-        db.data.menu.push(newMenu)
-        await db.write()
-
-        return newMenu
+    async create(menuData) {
+        return await Menu.create(menuData)
     }
 
-    static async getAll(){
-        await db.read()
-        return db.data.menu
+    async findAll(){
+        return await Menu.find()
     }
 
-    static async update(id, menuData){
-        await db.read()
-
-        const index = db.data.menu.findIndex(m=> m.id === id)
-
-        if(index === -1){
-            const error = new Error('Index não encontrado.')
-            error.statusCode = 404
-            throw error
-        }
-
-        const menuOriginal = db.data.menu[index]
-
-        const updateMenu = {
-            id: id,
-            nome: menuData.nome || menuOriginal.nome,
-            ingredientes: menuData.ingredientes || menuOriginal.ingredientes,
-            preco: menuData.preco || menuOriginal.preco,
-            observacoes: menuData.observacoes || menuOriginal.observacoes
-        }
-        
-        db.data.menu[index] = updateMenu
-
-        await db.write()
-
-        return updateMenu
+    async findById(id){
+        return await Menu.findById(id)
     }
 
-    static async delete(id){
-        await db.read()
-
-        const index = db.data.menu.findIndex(m => m.id === id)
-
-        if(index === -1){
-            const error = new Error('Index nao encontrado.')
-            error.statusCode = 404
-            throw error
-        }
-
-        const deletedMenu = db.data.menu.splice(index, 1)[0]
-        await db.write()
-
-        return deletedMenu
+    async update(id, updateData){
+        return await Menu.findByIdAndUpdate(id, updateData, {new: true})
     }
 
-    static async findById(id){
-        await db.read()
-        return db.data.menu.find(m=> m.id === id)
+    async delete(id){
+        return await Menu.findByIdAndDelete(id)
     }
 
-    static async findByName(nome) {
-        await db.read();
-        return db.data.menu.find(m => m.nome && m.nome.toLowerCase().trim() === nome.toLowerCase().trim());
-    }
 }
 
-export default MenuRepository
+export default new MenuRepository()
--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,103 +1,28 @@
-import db from "../config/dataBase.js";
-import {v4 as uuidv4} from 'uuid'
-import { env } from "../config/env.js";
-import bcrypt from 'bcrypt'
-
+import User from "../models/user.model.js"
 
 //Repositório dos itens do menu. Com as funções de criar, deletar, alterar, mostrar e achar por ID/Email.
 class UserRepository {
-    static async create(userData) {
-        await db.read()
-
-        const role = userData.role || "user"
-        
-        //Criptogrando a senha de novos usuários.
-        const salt = await bcrypt.genSalt(env.bcryptSaltRounds)
-        userData.senha = await bcrypt.hash(userData.senha, salt)
-
-        const newUser = {
-            id: uuidv4(),
-            nome: userData.nome,
-            email: userData.email, 
-            senha: userData.senha,
-            endereco: userData.endereco,
-            contato: userData.contato,
-            role: role
-        }
-
-        db.data.users.push(newUser)
-        await db.write()
-        
-        return newUser
+    
+    async create(userData) {
+        return await User.create(userData)
     }
 
-    static async getAll(){
-        await db.read()
-        return db.data.users
+    async findAll(){
+        return await User.find()
     }
 
-    static async update(id, updateData){
-        await db.read()
-        
-        const index = db.data.users.findIndex(u => u.id === id)
-
-        if(index === -1){
-            const error = new Error('Index nao encontrado.')
-            error.statusCode = 404
-            throw error
-        }
-
-        //Checando se informaram uma nova senha e criptografando-a.
-        if(updateData.senha){
-            const salt = await bcrypt.genSalt(env.bcryptSaltRounds)
-            updateData.senha = await bcrypt.hash(updateData.senha, salt)
-        }
-
-        const userOriginal = db.data.users[index]
-
-        const updateUser = {
-            id: id,
-            nome: updateData.nome || userOriginal.nome,
-            email: updateData.email || userOriginal.email, 
-            senha: updateData.senha || userOriginal.senha,
-            endereco: updateData.endereco || userOriginal.endereco,
-            contato: updateData.contato || userOriginal.contato,
-            role: userOriginal.role
-        }
-
-        db.data.users[index] = updateUser
-
-        await db.write()
-        
-        return updateUser
+    async findById(id){
+        return await User.findById(id)
     }
 
-    static async delete(id){
-        await db.read()
-
-        const index = db.data.users.findIndex(u => u.id === id)
-
-        if(index === -1){
-            const error = new Error('Index nao encontrado.')
-            error.statusCode = 404
-            throw error
-        }
-
-        const deletedUser = db.data.users.splice(index, 1)[0]
-        await db.write()
-
-        return deletedUser
+    async update(id, updateData){
+        return await User.findByIdAndUpdate(id, updateData, {new: true})
     }
 
-    static async findByEmail(email) {
-        await db.read();
-        return db.data.users.find(u => u.email && u.email.toLowerCase().trim() === email.toLowerCase().trim());
+    async delete(id){
+        return await User.findByIdAndDelete(id)
     }
 
-    static async findById(id){
-        await db.read()
-        return db.data.users.find(u=> u.id === id)
-    }
 }
 
-export default UserRepository
+export default new UserRepository()
